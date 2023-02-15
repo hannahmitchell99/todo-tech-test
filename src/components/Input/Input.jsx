894f52cd4d5b190b8d76ad05a580397928e25772
Hannah <hannahamitchell99@gmail.com>
2023-02-15 11:49:14 +0200

    added input box and button

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div className='input-container'>
+        <input className='input-container__input'></input>
+        <button className='input-container__add'>+</button>
+    </div>
+  )
+}
+
+export default Input
--- src/containers/Nav/Nav.jsx
+++ src/containers/Nav/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Input from '../../components/Input/Input'
 import ToDoHeading from '../../components/ToDoHeading/ToDoHeading'
 import "../Nav/Nav.scss"
 
 const Nav = () => {
   return (
     <nav className='nav-container'>Nav
-    <ToDoHeading/>
+        <ToDoHeading/>
+        <Input/>
     </nav>
   )
 }
