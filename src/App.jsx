53b54e2f976e9608211a83dc6467751d512548a7
Hannah <hannahamitchell99@gmail.com>
2023-02-15 12:12:44 +0200

    created to do item container

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.scss';
 
 import React from 'react'
 import Nav from './containers/Nav/Nav';
+import ToDoItemContainer from './containers/ToDoItemContainer/ToDoItemContainer';
 
 const App = () => {
   return (
     <div className='app'>
         <Nav/>
+        <ToDoItemContainer/>
     </div>
     
   )
---
+++ src/containers/ToDoItemContainer/ToDoItemContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../ToDoItemContainer/ToDoItemContainer.scss"
+
+const ToDoItemContainer = () => {
+  return (
+    <div className='item-container'>ToDoItemContainer</div>
+  )
+}
+
+export default ToDoItemContainer
